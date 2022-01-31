246c52f2298b8dba99dadebd606e2cb369174dfd
csh1603 <91582838+csh1603@users.noreply.github.com>
2022-01-31 14:23:02 +0900

    Create lastletter.java

---
+++ lastletter.java
@@ -0,0 +1,12 @@
+//글의 마지막 단어 출력
+public class Function2 {
+
+	public static char function(String input) {
+		return input.charAt(input.length() -1);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World의 마지막 단어는 "+function("Hello World")+"입니다.");
+	}
+
+}
